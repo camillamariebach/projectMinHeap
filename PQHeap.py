2ba4b6c1196dcf0c596af5f323d88e582cd720a3
camillamariebach <56353611+camillamariebach@users.noreply.github.com>
2020-03-09 18:19:31 +0000

    comments and sys.stdin added

--- PQHeap.py
+++ PQHeap.py
@@ -1,23 +1,18 @@
 import math
+import random
+import sys
 
-# find ud af hvordan man tager listen ind.
 
 def min_heapify(A, i):
-    print('I:' + str(A[i]))
+    """Method that restores the properties of a min heap by recursively calling itself on the list""" 
     l = left(i)
-    if l < len(A)-1:
-        print('L: ' + str(A[l])) 
     r = right(i)
-    if r < len(A)-1:
-        print('r:' + str(A[r]))
     if l <= len(A)-1 and A[l] < A[i]:
         smallest = l
     else: 
         smallest = i
-    print('smallest1: ' + str(A[smallest]))
     if r <= len(A)-1 and A[r] < A[smallest]:
         smallest = r
-    print('smallest2: ' + str(A[smallest]))
     if smallest != i: 
         temp = A[smallest]
         A[smallest] = A[i]
@@ -25,16 +20,19 @@ def min_heapify(A, i):
         min_heapify(A, smallest)
 
 def extractMin(A):
+    """Methods that extracts the minimum value in the heap and restores min heap properties""" 
     min = A[0]
     A[0] = A[len(A)-1]
     A.pop(len(A)-1)
     min_heapify(A, 0)
     return min
 
-def heap_min(A): 
+def heap_min(A):
+    """Method that returns the minimum value, stored in the node A[0]"""
     return A[0]
 
 def insert(A, e):
+    """Method that inserts an element to the end of the list A, then swaps something around""" 
     A.append(e)
     i = len(A)-1
     while i > 0 and A[parent(i)] > A[i]:
@@ -43,13 +41,14 @@ def insert(A, e):
         A[i] = temp
         i = parent(i)
 
-
-def build_min_heap(A): 
+def build_min_heap(A):
+    """Method that builds a min heap by recursively calling min_heapify on an unordered list"""
     m = math.floor((len(A)-1) / 2)
-    for i in range(m, -1, -1): 
+    for i in range(m, -1, -1):
         min_heapify(A, i)
 
 def heapsort(A):
+    """Method that manages the heap-sort algorithm by first building a heap, then outputs the list in sorted order and restores min heap properteis""" 
     build_min_heap(A)
     end = len(A)-1
     for i in range(end, 1, -1): 
@@ -59,16 +58,25 @@ def heapsort(A):
         min_heapify(A, 0)
  
 def parent(i):
+    """Getter for the parent node of i""" 
     return math.floor((i-1)/2)
 
 def left(i): 
+    """Getter for the left child node of i""" 
     return 2*i + 1
 
 def right(i):
+    """Getter for the right child node of i"""
     return 2*i + 2
 
-
-A = [56,64,234,8,11224,6,0,356,234,54432]
-
+A = sys.stdin()
 build_min_heap(A)
 print(A)
+sys.stdout(A)
+
+#Stuff for testing
+#A = [56,64,234,8,11224,6,0,356,234,54432,34,56,1234,578,356,25,-5, 0, -124, -356, 3346]
+#B = []
+#for i in range(200):
+#    b = random.randint(-1000, 1000)
+#    B.append(b)
