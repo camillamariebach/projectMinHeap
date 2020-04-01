baaae47be2b62302f61d4be068e6630af7492d4d
camillamariebach <56353611+camillamariebach@users.noreply.github.com>
2020-04-01 15:00:56 +0200

    more changes in insert

--- DictBinTree.py
+++ DictBinTree.py
@@ -21,25 +21,24 @@ def iterativeTreeSearch (T, k):
         else: T = T[2]
     return T
 
-def tinsert(T, k):
+def insert(T, k):
+    counter = 0
     y = None
     x = T[0]
     k = [k, None, None]
     
     while x != None:
         y = x
-        if k[0] < T[0][0]:
-            x = T[0][1]
-        else: x = T[0][2]
-        
-    #!!! hvad er p??
-    #k.p = y
-    
+        if k[0] < T[0+counter][0]:
+            x = T[0+counter][1]
+        else: x = T[0+counter][2]
+        counter += 1
+            
     if y == None:
-        T[0]=k
-    elif k[0] < y[0]:
-        y[1] = k
-    else: y[2] = k
+        T[0+counter]=k
+    elif y[1+counter] == None or k[0] < y[0+counter][0]:
+        y[1+counter][0] = k
+    else: y[2+counter][0] = k
     
 def orderedTraversal(T):
     inorderTreewalk(T[0])
@@ -51,7 +50,7 @@ def inorderTreewalk(T):
         orderedlist.append(T[0])
         inorderTreewalk(T[2])
 
-def insert(T, k):    
+def tinsert(T, k):    
     k = [k, None, None]
 
     while T[0] != None:
